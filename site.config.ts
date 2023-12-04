de1a4ddbe3c7616ac8c5ab4fc0f03fe0696e6307
YiduoLiu <152893821+YiduoLiu@users.noreply.github.com>
2023-12-04 10:20:46 -0500

    Personal Website Set Up

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,22 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3b6d0c5965b1482ba622b880233d97d4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'A Piece of Floating Cloud',
+  domain: 'https://continuous-bear-20b.notion.site/A-Piece-of-Floating-Cloud-3b6d0c5965b1482ba622b880233d97d4?pvs=4',
+  author: 'Yiduo Liu',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  Email: Lucreciayiduoliu@outlook.com
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
